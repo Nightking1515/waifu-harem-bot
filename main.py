a4640b312c2103e904d2354d7c74e4bcd871f488
Nightking1515 <app000916@gmail.com>
2025-05-31 16:17:38 +0530

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("Waifu Harem Bot Online ✅")
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.types import Message
+from aiogram.utils import executor
+import os
+
+API_TOKEN = os.getenv("BOT_TOKEN")  # token will come from Render environment
+
+# Enable logging
+logging.basicConfig(level=logging.INFO)
+
+# Initialize bot and dispatcher
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: Message):
+    await message.reply("Waifu bot is alive!")
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
