e03d86b8a0390479b04def5e7d794f0090f3e4b2
Nightking1515 <app000916@gmail.com>
2025-05-31 18:33:51 +0530

    main.py

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+import random
+import asyncio
+from telegram.ext import MessageHandler, filters
+import time
+
+# Waifu list (tum baad me isme aur add kar sakte ho)
+waifus = [
+    "Rem", "Emilia", "Zero Two", "Asuna", "Hinata", "Rias Gremory", "Kurumi Tokisaki", "Mikasa", "Nezuko", "Mai Sakurajima"
+]
+
+# Mode and timers
+spawn_mode = "time"  # "time" or "message"
+spawn_interval = 120  # seconds (2 minutes)
+message_count_target = 100
+current_message_count = 0
+last_spawn_time = 0
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
