12e22d81a0b5e2835c5ed9bb48aa834ae84af8f8
Nightking1515 <app000916@gmail.com>
2025-05-31 17:57:59 +0530

    main.py

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ BOT_TOKEN = os.environ.get("7692706456:AAEB7jphJNSOpbBl7bzxSnausRZ01viIEbY")
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Hello! Waifu Harem Bot is live!")
 
-app = ApplicationBuilder().token(BOT_TOKEN).build()
+app = ApplicationBuilder().token(7692706456:AAEB7jphJNSOpbBl7bzxSnausRZ01viIEbY).build()
 app.add_handler(CommandHandler("start", start))
 app.run_polling()
-
