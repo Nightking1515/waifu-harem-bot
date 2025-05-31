42fc81048cb7c7ee67e477f230687d817c9fe8c1
Nightking1515 <app000916@gmail.com>
2025-05-31 17:31:04 +0530

    main.py

--- main.py
+++ main.py
@@ -1,20 +1,13 @@
-import logging
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import os
 
-TOKEN = "7692706456:AAEB7jphJNSOpbBl7bzxSnausRZ01viIEbY"
-
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
-)
+BOT_TOKEN = os.environ.get("7692706456:AAEB7jphJNSOpbBl7bzxSnausRZ01viIEbY")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Hello! Waifu Harem Bot is live!")
 
-if __name__ == '__main__':
-    app = ApplicationBuilder().token(TOKEN).build()
+app = ApplicationBuilder().token(BOT_TOKEN).build()
+app.add_handler(CommandHandler("start", start))
+app.run_polling()
 
-    app.add_handler(CommandHandler("start", start))
-
-    app.run_polling()
